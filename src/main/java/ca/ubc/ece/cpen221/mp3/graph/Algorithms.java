951a7f4bc6708432dad84e07d1412b48e5865441
Randy Jama <randyjama@gmail.com>
2017-10-23 18:34:42 -0700

    MP3 submission

--- src/main/java/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
+++ src/main/java/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
@@ -1,7 +1,12 @@
 package ca.ubc.ece.cpen221.mp3.graph;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
 
 import ca.ubc.ece.cpen221.mp3.staff.Graph;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
@@ -12,59 +17,170 @@ public class Algorithms {
 	 * *********************** Algorithms ****************************
 	 *
 	 * Please see the README for this machine problem for a more detailed
-	 * specification of the behavior of each method that one should
-	 * implement.
+	 * specification of the behavior of each method that one should implement.
 	 */
 
 	/**
-	 * This is provided as an example to indicate that this method and
-	 * other methods should be implemented here.
+	 * This is provided as an example to indicate that this method and other methods
+	 * should be implemented here.
 	 *
 	 * You should write the specs for this and all other methods.
 	 *
 	 * @param graph
 	 * @param a
+	 *            Vertex must exist within the graph
 	 * @param b
+	 *            Vertex must exist within the graph
 	 * @return
 	 */
 	public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
 		// TODO: Implement this method and others
-		return 0;
+		// if list starting with a does not contain b, then no link exists, return -1
+		for (List<Vertex> list : Algorithms.breadthFirstSearch(graph)) {
+			if (list.indexOf(a) == 0) {
+				if (!list.contains(b))
+					return -1;
+			}
+		}
+		// other base case for no distance
+		if (a == b)
+			return 0;
+
+		// begin creating path from a to b
+		int distance = 0;
+		List<Vertex> nextDownStream = new ArrayList<Vertex>();
+		List<Vertex> downStream = new ArrayList<Vertex>();
+		List<Vertex> allDownStreams = new ArrayList<Vertex>(); // used for tracking already visited nodes to prevent
+																// cycles
+		downStream.add(a);
+		allDownStreams.add(a);
+
+		while (!downStream.contains(b) && !downStream.isEmpty()) {
+			distance++;
+			for (Vertex vertex : downStream) {
+				nextDownStream.addAll(graph.getDownstreamNeighbors(vertex));
+				allDownStreams.addAll(downStream);
+			}
+
+			// check and remove all common vertices from nextDownStream if already present
+			// in allDownStreams
+			for (Vertex vertex : allDownStreams) {
+				if (nextDownStream.contains(vertex))
+					nextDownStream.remove(vertex);
+			}
+
+			downStream.clear();
+			downStream.addAll(nextDownStream);
+			nextDownStream.clear();
+		}
+
+		return distance;
 	}
 
 	/**
-	 * Perform a complete depth first search of the given
-	 * graph. Start with the search at each vertex of the
-	 * graph and create a list of the vertices visited.
-	 * Return a set where each element of the set is a list
-	 * of elements seen by starting a DFS at a specific
-	 * vertex of the graph (the number of elements in the
-	 * returned set should correspond to the number of graph
-	 * vertices).
+	 * Perform a complete depth first search of the given graph. Start with the
+	 * search at each vertex of the graph and create a list of the vertices visited.
+	 * Return a set where each element of the set is a list of elements seen by
+	 * starting a DFS at a specific vertex of the graph (the number of elements in
+	 * the returned set should correspond to the number of graph vertices).
 	 *
 	 * @param
 	 * @return
 	 */
 	public static Set<List<Vertex>> depthFirstSearch(Graph graph) {
 		// TODO: Implement this method
-		return null; // this should be changed
+
+		Set<List<Vertex>> resultBFS = new HashSet<List<Vertex>>();
+
+		// vertexList holds all vertices to do DFS on
+		List<Vertex> vertexList = new ArrayList<Vertex>();
+		vertexList.addAll(graph.getVertices());
+
+		// list contains DFS results for a given vertex (later added to resultBFS)
+		List<Vertex> list = new ArrayList<Vertex>();
+
+		for (int i = 0; i < vertexList.size(); i++) {
+			list.clear();
+			Stack<Vertex> S = new Stack<Vertex>();
+			S.push(vertexList.get(i));
+			// boolean[] discovered = new boolean [vertexList.size()];
+			while (!S.empty()) {
+				Vertex v = S.pop();
+				// if list doesn't contain v, add it to list and add DSNs to stack
+				if (!list.contains(v)) {
+					list.add(v);
+					for (Vertex j : graph.getDownstreamNeighbors(v)) {
+						S.push(j);
+					}
+
+				}
+			}
+			// need to create a new list to store results of vertex's search for diff memory
+			// locations
+			List<Vertex> addingList = new ArrayList<Vertex>(list);
+			resultBFS.add(addingList);
+		}
+
+		return resultBFS; // this should be changed
 
 	}
 
 	/**
-	 * Perform a complete breadth first search of the given
-	 * graph. Start with the search at each vertex of the
-	 * graph and create a list of the vertices visited.
-	 * Return a set where each element of the set is a list
-	 * of elements seen by starting a BFS at a specific
-	 * vertex of the graph (the number of elements in the
-	 * returned set should correspond to the number of graph
-	 * vertices).
+	 * Perform a complete breadth first search of the given graph. Start with the
+	 * search at each vertex of the graph and create a list of the vertices visited.
+	 * Return a set where each element of the set is a list of elements seen by
+	 * starting a BFS at a specific vertex of the graph (the number of elements in
+	 * the returned set should correspond to the number of graph vertices).
 	 *
-	 * @param
-	 * @return
+	 * @param graph
+	 *            must have correct implementation of graph interface
+	 * @return set's # elements = # vertices
 	 */
 	public static Set<List<Vertex>> breadthFirstSearch(Graph graph) {
+		// TODO: Implement this method
+
+		Set<List<Vertex>> resultBFS = new HashSet<List<Vertex>>();
+		// set and initialize capacity of vertexList, then fill the list with vertices
+		List<Vertex> vertexList = new ArrayList<Vertex>();
+		vertexList.addAll(graph.getVertices());
+
+		// list contains children of given vertex
+		List<Vertex> list = new ArrayList<Vertex>();
+
+		// do BFS for all vertices
+		for (int k = 0; k < graph.getVertices().size(); k++) {
+			list.clear();
+			list.add(vertexList.get(k));
+
+			Queue<Vertex> queue = new LinkedList<Vertex>();
+
+			// throw 1st parent vertex into queue
+			queue.offer(vertexList.get(k));
+			while (!queue.isEmpty()) {
+				Vertex v = queue.poll();
+
+				// add all UNREPEATED DS neighbours to the queue
+				for (int i = 0; i < graph.getDownstreamNeighbors(v).size(); i++) {
+					// if entry of list does NOT contain DSN, add it to list and queue
+					if (!list.contains(graph.getDownstreamNeighbors(v).get(i))) {
+						list.add(graph.getDownstreamNeighbors(v).get(i));
+						queue.offer(graph.getDownstreamNeighbors(v).get(i));
+					}
+				}
+			}
+			// need to create a new list to store results of vertex's search for diff memory
+			// locations
+			List<Vertex> addingList = new ArrayList<Vertex>(list);
+			resultBFS.add(addingList);
+		}
+
+		return resultBFS;
+	}
+
+	/**
+	 * You should write the spec for this method
+	 */
+	public static Vertex center(Graph graph) {
 		// TODO: Implement this method
 		return null; // this should be changed
 	}
@@ -72,33 +188,26 @@ public class Algorithms {
 	/**
 	 * You should write the spec for this method
 	 */
-	 public static Vertex center(Graph graph) {
-		 // TODO: Implement this method
-		 return null; // this should be changed
-	 }
-
-	 /**
-	  * You should write the spec for this method
-		*/
-		public static int diameter(Graph graph) {
-			// TODO: Implement this method
-			return -1; // this should be changed
-		}
+	public static int diameter(Graph graph) {
+		// TODO: Implement this method
+		
+		return -1; // this should be changed
+	}
 
 	/**
 	 * You should write the spec for this method
 	 */
-	 public static List<Vertex> commonUpstreamVertices(Graph graph, Vertex a, Vertex b) {
-		 // TODO: Implement this method
- 		return null; // this should be changed
-	 }
+	public static List<Vertex> commonUpstreamVertices(Graph graph, Vertex a, Vertex b) {
+		// TODO: Implement this method
+		return null; // this should be changed
+	}
 
-	 /**
- 	 * You should write the spec for this method
- 	 */
- 	 public static List<Vertex> commonDownstreamVertices(Graph graph, Vertex a, Vertex b) {
- 		 // TODO: Implement this method
-  		return null; // this should be changed
- 	 }
+	/**
+	 * You should write the spec for this method
+	 */
+	public static List<Vertex> commonDownstreamVertices(Graph graph, Vertex a, Vertex b) {
+		// TODO: Implement this method
+		return null; // this should be changed
+	}
 
 }
--- src/test/java/tests/adjTest.java
+++ src/test/java/tests/adjTest.java
@@ -2,125 +2,244 @@ package tests;
 
 import static org.junit.Assert.*;
 
-import java.awt.List;
+import java.util.List;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
 import ca.ubc.ece.cpen221.mp3.graph.AdjacencyListGraph;
 import ca.ubc.ece.cpen221.mp3.graph.AdjacencyMatrixGraph;
+import ca.ubc.ece.cpen221.mp3.graph.Algorithms;
 import ca.ubc.ece.cpen221.mp3.staff.Graph;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class adjTest {
 
 	@Test
-	//test addVertex method
-	public void test0() throws IOException {
-		Vertex v1 = new Vertex("Dante"); //dante vergil linked both ways
+	// test addVertex method
+	public void adjListTest() throws IOException {
+		Vertex v1 = new Vertex("Dante"); // dante vergil linked both ways
 		Vertex v2 = new Vertex("Vergil");
 		Vertex v3 = new Vertex("Trish");// dante to trish
-		Vertex v4 = new Vertex("Megaman"); //megaman zero linked both ways
+		Vertex v4 = new Vertex("Megaman"); // megaman zero linked both ways
 		Vertex v5 = new Vertex("Zero");
-		Vertex v6 = new Vertex("Link"); //link is unmatched
-		
+		Vertex v6 = new Vertex("Link"); // link is unmatched
+
 		Graph adjGraph = new AdjacencyListGraph();
-		
+
 		adjGraph.addVertex(v1);
 		adjGraph.addVertex(v2);
 		adjGraph.addVertex(v3);
 		adjGraph.addVertex(v4);
 		adjGraph.addVertex(v5);
 		adjGraph.addVertex(v6);
-		
+
 		adjGraph.addEdge(v1, v2);
 		adjGraph.addEdge(v2, v1);
 		adjGraph.addEdge(v1, v3);
 		adjGraph.addEdge(v4, v5);
 		adjGraph.addEdge(v5, v4);
-		
-		//test list of vertices
-		assert(adjGraph.getVertices().contains(v1));
-		assert(adjGraph.getVertices().contains(v2));
-		assert(adjGraph.getVertices().contains(v3));
-		assert(adjGraph.getVertices().contains(v4));
-		assert(adjGraph.getVertices().contains(v5));
-		assert(adjGraph.getVertices().contains(v6));
-		
-		//test edges
-		assert(adjGraph.edgeExists(v1, v2));
-		assert(adjGraph.edgeExists(v2, v1));
-		assert(adjGraph.edgeExists(v1, v3));
-		assert(adjGraph.edgeExists(v4, v5));
-		assert(adjGraph.edgeExists(v5, v4));
-		assertFalse(adjGraph.edgeExists(v1, v6)); //untrue edge
-		
-		//test downstream neighbours
-		assert(adjGraph.getDownstreamNeighbors(v1).contains(v2)); //Dante -> Vergil
-		assert(adjGraph.getDownstreamNeighbors(v1).contains(v3)); //Dante -> Trish
-		assertFalse(adjGraph.getDownstreamNeighbors(v1).contains(v5)); //Dante does NOT link Zero
-		assert(adjGraph.getDownstreamNeighbors(v4).contains(v5)); //Megaman -> Zero
-		
-		//test upstream neighbours
-		assert(adjGraph.getUpstreamNeighbors(v1).contains(v2)); //Dante <- Vergil
-		assertFalse(adjGraph.getUpstreamNeighbors(v1).contains(v3)); //dante <-/- Trish
-		assert(adjGraph.getUpstreamNeighbors(v4).contains(v5)); //Megaman <- Zero
-		
+
+		// test list of vertices
+		assert (adjGraph.getVertices().contains(v1));
+		assert (adjGraph.getVertices().contains(v2));
+		assert (adjGraph.getVertices().contains(v3));
+		assert (adjGraph.getVertices().contains(v4));
+		assert (adjGraph.getVertices().contains(v5));
+		assert (adjGraph.getVertices().contains(v6));
+
+		// test edges
+		assert (adjGraph.edgeExists(v1, v2));
+		assert (adjGraph.edgeExists(v2, v1));
+		assert (adjGraph.edgeExists(v1, v3));
+		assert (adjGraph.edgeExists(v4, v5));
+		assert (adjGraph.edgeExists(v5, v4));
+		assertFalse(adjGraph.edgeExists(v1, v6)); // untrue edge
+		assertFalse(adjGraph.edgeExists(v1, v1)); // untrue edge
+
+		// test downstream neighbours
+		assert (adjGraph.getDownstreamNeighbors(v1).contains(v2)); // Dante -> Vergil
+		assert (adjGraph.getDownstreamNeighbors(v1).contains(v3)); // Dante -> Trish
+		assertFalse(adjGraph.getDownstreamNeighbors(v1).contains(v5)); // Dante does NOT link Zero
+		assert (adjGraph.getDownstreamNeighbors(v4).contains(v5)); // Megaman -> Zero
+
+		// test upstream neighbours
+		assert (adjGraph.getUpstreamNeighbors(v1).contains(v2)); // Dante <- Vergil
+		assertFalse(adjGraph.getUpstreamNeighbors(v1).contains(v3)); // dante <-/- Trish
+		assert (adjGraph.getUpstreamNeighbors(v4).contains(v5)); // Megaman <- Zero
 
 	}
-	
+
 	@Test
-	public void test1() throws IOException {
-		Vertex v1 = new Vertex("Dante"); //dante vergil linked both ways
+	public void adjMatrixTest() throws IOException {
+		Vertex v1 = new Vertex("Dante"); // dante vergil linked both ways
 		Vertex v2 = new Vertex("Vergil");
-		Vertex v3 = new Vertex("Trish");// dante to trish
-		Vertex v4 = new Vertex("Megaman"); //megaman zero linked both ways
+		Vertex v3 = new Vertex("Trish"); // dante to trish
+		Vertex v4 = new Vertex("Megaman"); // megaman zero linked both ways
 		Vertex v5 = new Vertex("Zero");
-		//Vertex v6 = new Vertex("Link"); //link is unmatched
-		
+		// Vertex v6 = new Vertex("Link"); //link is unmatched
+
 		Graph adjGraph = new AdjacencyMatrixGraph(5);
-		
+
 		adjGraph.addVertex(v1);
 		adjGraph.addVertex(v2);
 		adjGraph.addVertex(v3);
 		adjGraph.addVertex(v4);
 		adjGraph.addVertex(v5);
-		//adjGraph.addVertex(v6);
-		
+		// adjGraph.addVertex(v6);
+
 		adjGraph.addEdge(v1, v2);
 		adjGraph.addEdge(v2, v1);
 		adjGraph.addEdge(v1, v3);
 		adjGraph.addEdge(v4, v5);
 		adjGraph.addEdge(v5, v4);
-		
-		//test list of vertices
-		assert(adjGraph.getVertices().contains(v1));
-		assert(adjGraph.getVertices().contains(v2));
-		assert(adjGraph.getVertices().contains(v3));
-		assert(adjGraph.getVertices().contains(v4));
-		assert(adjGraph.getVertices().contains(v5));
-		//assert(adjGraph.getVertices().contains(v6));
 
-		//test edges
-		assert(adjGraph.edgeExists(v1, v2));
-		assert(adjGraph.edgeExists(v2, v1));
-		assert(adjGraph.edgeExists(v1, v3));
-		assert(adjGraph.edgeExists(v4, v5));
-		assert(adjGraph.edgeExists(v5, v4));
-		assertFalse(adjGraph.edgeExists(v1, v5)); //untrue edge
-		
-		//test downstream neighbours
-		assert(adjGraph.getDownstreamNeighbors(v1).contains(v2)); //Dante -> Vergil
-		assert(adjGraph.getDownstreamNeighbors(v1).contains(v3)); //Dante -> Trish
-		assertFalse(adjGraph.getDownstreamNeighbors(v1).contains(v5)); //Dante does NOT link Zero
-		assert(adjGraph.getDownstreamNeighbors(v4).contains(v5)); //Megaman -> Zero
-		
-		//test upstream neighbours
-		assert(adjGraph.getUpstreamNeighbors(v1).contains(v2)); //Dante <- Vergil
-		assertFalse(adjGraph.getUpstreamNeighbors(v1).contains(v3)); //dante <-/- Trish
-		assert(adjGraph.getUpstreamNeighbors(v4).contains(v5)); //Megaman <- Zero
+		// test list of vertices
+		assert (adjGraph.getVertices().contains(v1));
+		assert (adjGraph.getVertices().contains(v2));
+		assert (adjGraph.getVertices().contains(v3));
+		assert (adjGraph.getVertices().contains(v4));
+		assert (adjGraph.getVertices().contains(v5));
+		// assert(adjGraph.getVertices().contains(v6));
+
+		// test edges
+		assert (adjGraph.edgeExists(v1, v2));
+		assert (adjGraph.edgeExists(v2, v1));
+		assert (adjGraph.edgeExists(v1, v3));
+		assert (adjGraph.edgeExists(v4, v5));
+		assert (adjGraph.edgeExists(v5, v4));
+		assertFalse(adjGraph.edgeExists(v1, v5)); // untrue edge
+		assertFalse(adjGraph.edgeExists(v1, v1)); // untrue edge
+
+		// test downstream neighbours
+		assert (adjGraph.getDownstreamNeighbors(v1).contains(v2)); // Dante -> Vergil
+		assert (adjGraph.getDownstreamNeighbors(v1).contains(v3)); // Dante -> Trish
+		assertFalse(adjGraph.getDownstreamNeighbors(v1).contains(v5)); // Dante does NOT link Zero
+		assert (adjGraph.getDownstreamNeighbors(v4).contains(v5)); // Megaman -> Zero
+
+		// test upstream neighbours
+		assert (adjGraph.getUpstreamNeighbors(v1).contains(v2)); // Dante <- Vergil
+		assertFalse(adjGraph.getUpstreamNeighbors(v1).contains(v3)); // dante <-/- Trish
+		assert (adjGraph.getUpstreamNeighbors(v4).contains(v5)); // Megaman <- Zero
 	}
 
+	@Test
+	public void bfsTest() throws IOException {
+		Vertex v1 = new Vertex("Dante"); // dante vergil linked both ways
+		Vertex v2 = new Vertex("Vergil");
+		Vertex v3 = new Vertex("Trish"); // dante to trish
+		Vertex v4 = new Vertex("Megaman"); // megaman zero linked both ways
+		Vertex v5 = new Vertex("Zero");
+		Vertex v7 = new Vertex("Blue");
+		// Vertex v6 = new Vertex("Link"); //link is unmatched
+
+		Graph adjGraph = new AdjacencyMatrixGraph(6);
+
+		// Set<List> setCompare = new HashSet<List>();
+		// Algorithms alg = new Algorithms();
+
+		adjGraph.addVertex(v1);
+		adjGraph.addVertex(v2);
+		adjGraph.addVertex(v3);
+		adjGraph.addVertex(v4);
+		adjGraph.addVertex(v5);
+		adjGraph.addVertex(v7);
+		// adjGraph.addVertex(v6);
+
+		adjGraph.addEdge(v1, v2);
+		adjGraph.addEdge(v2, v1);
+		adjGraph.addEdge(v1, v3);
+		adjGraph.addEdge(v4, v5);
+		adjGraph.addEdge(v5, v4);
+		adjGraph.addEdge(v2, v7);
+
+		Set<List<Vertex>> setCompare = new HashSet<>();
+		setCompare = Algorithms.breadthFirstSearch(adjGraph);
+
+		assert (setCompare.size() == 6);
+	}
+
+	@Test
+	public void dfsTest() throws IOException {
+		Vertex v1 = new Vertex("Dante"); // dante vergil linked both ways
+		Vertex v2 = new Vertex("Vergil");
+		Vertex v3 = new Vertex("Trish"); // dante to trish
+		Vertex v4 = new Vertex("Megaman"); // megaman zero linked both ways
+		Vertex v5 = new Vertex("Zero");
+		Vertex v7 = new Vertex("Blue");
+		// Vertex v6 = new Vertex("Link"); //link is unmatched
+
+		Graph adjGraph = new AdjacencyMatrixGraph(6);
+
+		// Set<List> setCompare = new HashSet<List>();
+		// Algorithms alg = new Algorithms();
+
+		adjGraph.addVertex(v1);
+		adjGraph.addVertex(v2);
+		adjGraph.addVertex(v3);
+		adjGraph.addVertex(v4);
+		adjGraph.addVertex(v5);
+		adjGraph.addVertex(v7);
+		// adjGraph.addVertex(v6);
+
+		adjGraph.addEdge(v1, v2);
+		adjGraph.addEdge(v2, v1);
+		adjGraph.addEdge(v1, v3);
+		adjGraph.addEdge(v4, v5);
+		adjGraph.addEdge(v5, v4);
+		adjGraph.addEdge(v2, v7);
+
+		Set<List<Vertex>> setCompare = new HashSet<>();
+		setCompare = Algorithms.depthFirstSearch(adjGraph);
+
+		assert (setCompare.size() == 6);
+	}
+
+	@Test
+	public void distanceTest() throws IOException {
+		Vertex v1 = new Vertex("1");
+		Vertex v2 = new Vertex("2");
+		Vertex v3 = new Vertex("3");
+		Vertex v4 = new Vertex("4");
+		Vertex v5 = new Vertex("5");
+		Vertex v6 = new Vertex("6");
+		Vertex v7 = new Vertex("7");
+		Vertex v8 = new Vertex("8");
+
+		
+		Graph graph = new AdjacencyListGraph();
+		
+		graph.addVertex(v1);
+		graph.addVertex(v2);
+		graph.addVertex(v3);
+		graph.addVertex(v4);
+		graph.addVertex(v5);
+		graph.addVertex(v6);
+		graph.addVertex(v7);
+		graph.addVertex(v8);
+
+		graph.addEdge(v1, v4);
+		graph.addEdge(v1, v2);
+		graph.addEdge(v2, v1);
+		graph.addEdge(v2, v4);
+		graph.addEdge(v4, v5);
+		graph.addEdge(v2, v3);
+		graph.addEdge(v2, v6);
+		graph.addEdge(v6, v2);
+		graph.addEdge(v4, v7);
+		graph.addEdge(v7, v4);
+		graph.addEdge(v7, v8);
+		
+		assertEquals(2, Algorithms.shortestDistance(graph, v1, v3));
+		assertEquals(3, Algorithms.shortestDistance(graph, v1, v8));
+		assertFalse(Algorithms.shortestDistance(graph, v6, v8) == 2);
+		assertEquals(4, Algorithms.shortestDistance(graph, v6, v8));
+		assertFalse(Algorithms.shortestDistance(graph, v5, v6) == 3);
+		assertEquals(2, Algorithms.shortestDistance(graph, v7, v5));
+	}
+	
 }
